5c77d99ae39043bb69d4b7b9c885d6d22783d348
Jonathan Pastor <jonathancmoa@gmail.com>
2018-11-21 15:57:18 +0100

    specify address of peer in args

--- src/main/java/distributed/Main.java
+++ src/main/java/distributed/Main.java
@@ -20,7 +20,7 @@ public class Main {
         System.out.println("Launching client");
 
         // make a Config with just your special setting
-        Config myConfig = ConfigFactory.parseString(ConfigurationGenerator.generateConfig("127.0.0.1", args[1]));
+        Config myConfig = ConfigFactory.parseString(ConfigurationGenerator.generateConfig(args[1], args[2]));
 
         // load the normal config stack (system props, then application.conf, then reference.conf)
         Config regularConfig = ConfigFactory.load();
@@ -37,8 +37,8 @@ public class Main {
         final ActorRef greeter1 =
                 system.actorOf(Peer.props("greeter2>"), "greeter2");
 
-        String remote_address = args[2];
-        String remote_port = args[3];
+        String remote_address = args[3];
+        String remote_port = args[4];
 
         ActorSelection selection =
                 system.actorSelection("akka.tcp://hello_akka@"+remote_address+":"+remote_port+"/user/greeter1");
@@ -50,8 +50,9 @@ public class Main {
         System.out.println("Launching server");
 
         // make a Config with just your special setting
-        String port = args[1];
-        Config myConfig = ConfigFactory.parseString(ConfigurationGenerator.generateConfig("127.0.0.1", port));
+        String address = args[1];
+        String port = args[2];
+        Config myConfig = ConfigFactory.parseString(ConfigurationGenerator.generateConfig(address, port));
 
         // load the normal config stack (system props, then application.conf, then reference.conf)
         Config regularConfig = ConfigFactory.load();
@@ -74,14 +75,14 @@ public class Main {
 //        args1[0] = "server";
 //        args1[1] = "1234";
 //        args = args1;
-        if (args.length == 2 && args[0].equals("server")) {
+        if (args.length == 3 && args[0].equals("server")) {
             server(args);
-        } else if (args.length == 4 && args[0].equals("client")) {
+        } else if (args.length == 5 && args[0].equals("client")) {
             client(args);
         } else {
             System.out.println("Run the program with the following arguments:");
-            System.out.println("  * java Main server <local_port>");
-            System.out.println("  * java Main client <local_port> <remote_address> <remote_port>");
+            System.out.println("  * java Main server <local_address> <local_port>");
+            System.out.println("  * java Main client <local_address> <local_port> <remote_address> <remote_port>");
         }
     }
 }
